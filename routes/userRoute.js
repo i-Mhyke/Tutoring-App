bf541467ac6664fffcce5d367dd29185dd94eabf
i-Mhyke <58915377+i-Mhyke@users.noreply.github.com>
2020-05-07 22:33:48 +0100

    user update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const userController = require('./../controllers/userController');
+const lessonController = require('./../controllers/lessonController');
 const router = express.Router();
 
 router.post('/signup', authController.signUp);
@@ -26,6 +27,25 @@ router.delete('/tutor/subjects/:subject_id',
                 authController.protectRoutes, 
                 authController.restrictTo('tutor'), 
                 userController.deleteRegisteredSubject);
+router.post('/admin/lesson', 
+                authController.protectRoutes, 
+                authController.restrictToAdmin, 
+                lessonController.adminCreateLesson);
+router.post('/student/lesson', 
+                authController.protectRoutes, 
+                authController.restrictTo("user"), 
+                lessonController.studentCreateLesson);
+router.get('/student/lessons', authController.protectRoutes, lessonController.studentLessons);
+router.get('/tutor/lessons', authController.protectRoutes, lessonController.tutorLessons);
+router.get('/lessons', authController.protectRoutes, authController.restrictToAdmin, lessonController.getAllLessons);
+router.put('/lessons/:lesson_id', 
+                authController.protectRoutes, 
+                authController.restrictToAdmin, 
+                lessonController.updateLesson);
+router.delete('/lesson/:lesson_id', 
+                authController.protectRoutes, 
+                authController.restrictToAdmin, 
+                lessonController.deleteLesson);
 //router.param('user_id', userController.userById);
 
 module.exports = router;
