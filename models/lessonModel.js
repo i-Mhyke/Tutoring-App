4785f706b16657c877399aa8737ab4a831eb0c79
i-Mhyke <58915377+i-Mhyke@users.noreply.github.com>
2020-05-07 22:32:29 +0100

    Lesson integration module

--- models/lessonModel.js
+++ models/lessonModel.js
@@ -1,20 +1,43 @@
 const mongoose = require('mongoose');
+const User = require('./userModel');
 
 const lessonSchema = new mongoose.Schema({
-    topic: {
-        type: String,
-        uppercase: true
-    },
     subject: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Subject"
+        type: String,
+    },
+    tutor_email:{
+        type: String,
+        required: [true, "input tutor email"],
+        lowercase: true,
+        select: false
     },
     tutor: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
+    },
+    student_email:{
+        type: String,
+        required: [true, "input student's email"],
+        lowercase: true,
+        select: false
+    },
+    student: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
     }
-});
+},{timestamps: true});
 
+// lessonSchema.post('save', async function(next){
+//     const tutor = await User.findOne({email: this.tutor_email});
+//     const student = await User.findOne({email: this.student_email});
 
+//     await this.update({_id: req.params.lesson_id},  {
+//         $set: {
+//           "tutor": tutor.email,
+//           "student": student.email
+//         }
+//       })
+//       next();
+// })
 const Lesson = mongoose.model('Lesson', lessonSchema);
 module.exports = Lesson;
